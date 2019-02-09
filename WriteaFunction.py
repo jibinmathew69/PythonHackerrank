745a384a7ca13cbe4f9f80607a07ce688f2c4ad6
jibinmathew691993 <jibinmathew.69@gmail.com>
2019-02-09 15:49:58 +0530

    Add Write a function

---
+++ WriteaFunction.py
@@ -0,0 +1,12 @@
+def is_leap(year):
+    leap = False
+    if year>=1900:
+        if year%4 == 0:
+            leap = True
+            if year%100 == 0 and year%400 != 0:
+                leap = False
+
+    return leap
+
+
+year = int(input())
