18128636e1c285289abb6aa54703fc2f59ce301b
jibinmathew691993 <jibinmathew.69@gmail.com>
2019-02-13 13:33:39 +0530

    Update Capitalize

--- Capitalize.py
+++ Capitalize.py
@@ -1,6 +1,13 @@
+# def solve(s):
+#     names = s.split()
+#     return " ".join([name[0].upper()+name[1:] for name in names])
+# the above methods compromises on the spacing between the words
+
+import string
 def solve(s):
-    names = s.split()
-    return " ".join([name[0].upper()+name[1:] for name in names])
+    return string.capwords(s, ' ')
+
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
