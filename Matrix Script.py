cd309f62b9cccc64bcf5d26be4019b99e8b481a4
jibinmathew691993 <jibinmathew.69@gmail.com>
2019-02-23 14:59:23 +0530

    Add Matrix Script

---
+++ Matrix Script.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import operator
+from functools import reduce
+
+nm = input().split()
+
+n = int(nm[0])
+
+m = int(nm[1])
+
+matrix = []
+
+for _ in range(n):
+    matrix_item = list(input())
+    matrix.append(matrix_item)
+
+string = "".join(reduce(operator.concat, [list(i) for i in zip(*matrix)]))
+print(re.sub(r"(?<=\w)([^\w]+)(?=\w)", " ", string))
